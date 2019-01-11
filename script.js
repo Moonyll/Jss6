a7fcb749e1d8c83d56c459fc085fd8c6c0bdc8ad
Moonyll <s.pouwel@laposte.net>
2019-01-11 09:41:49 +0100

    Jss6

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿function chg() {
+    document.getElementById('image1').src = "assets/image1_2.jpg";
+    document.getElementById('image2').src = "assets/image2_2.jpg";
+    document.getElementById('image3').src = "assets/image3_2.jpg";
+    document.getElementById('image4').src = "assets/image4_2.jpg";
+    document.getElementById('image5').src = "assets/image5_2.jpg";
+}
+function ini() {
+    document.getElementById('image1').src = "assets/image1.jpg";
+    document.getElementById('image2').src = "assets/image2.jpg";
+    document.getElementById('image3').src = "assets/image3.jpg";
+    document.getElementById('image4').src = "assets/image4.jpg";
+    document.getElementById('image5').src = "assets/image5.jpg";
+
+}
